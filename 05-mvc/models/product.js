a7a97bd9ac62494a09a0b4126b5b0f508db2ff2e
Hadar4476 <hadar4476@gmail.com>
2024-12-21 11:28:46 +0200

    changes

--- 05-mvc/models/product.js
+++ 05-mvc/models/product.js
@@ -1,9 +1,28 @@
-const fs = require('fs');
-const path = require('path');
+// readFile/writeFile vs. readFileSync/writeFileSync
 
-const rootDir = require('../utils/path');
+// readFile/writeFile:
+// - an asynchronous operation which happens in the background without blocking any code.
+// - suitable for high-performance and scalable apps.
+// - DOES NOT BLOCK THE EVENT LOOP.
+// - good for scenarios when:
+//   * writing a server-side apps where blocking the event loop could degrade performance.
+//   * multiple file operations should be handled.
 
-const filePath = path.join(rootDir, 'data', 'products.json');
+// readFileSync/writeFileSync:
+// - a synchronous operation which blocks the code until its complete.
+// - not recommanded for performance-critical or asynchronous enviroments, like handling HTTP requests.
+// - BLOCKS THE EVENT LOOP.
+// - good for scenerios when:
+//   * simplicity is more important than performance.
+//   * operation completion must be ensured before proceeding.
+//   * performing file operations during the application startup(e.g, loading configuration files).
+
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../utils/path");
+
+const filePath = path.join(rootDir, "data", "products.json");
 
 const getProductsFromJSON = (callback) => {
   fs.readFile(filePath, (err, data) => {
